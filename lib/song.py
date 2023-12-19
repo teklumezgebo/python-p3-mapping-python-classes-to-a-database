0956b6ec5b336f5666612ede090d51648a3edacb
Teklu Mezgebo <mezge002@umn.edu>
2023-12-19 01:13:29 -0600

    assignment finished

--- lib/song.py
+++ lib/song.py
@@ -1,6 +1,8 @@
 from config import CONN, CURSOR
 
 class Song:
+
+    all = []
     
     def __init__(self, name, album):
         self.id = None
@@ -36,3 +38,33 @@ class Song:
         song = Song(name, album)
         song.save()
         return song
+    
+    @classmethod
+    def new_from_db(cls, row):
+        song = cls(row[1], row[2])
+        song.id = row[0]
+        return song
+    
+    @classmethod
+    def get_all(cls):
+        sql = """
+            SELECT *
+            FROM songs
+        """
+        
+        all = CURSOR.execute(sql).fetchall()
+
+        cls.all = [cls.new_from_db(row) for row in all]
+
+    @classmethod
+    def find_by_name(cls, name):
+        sql = """
+        SELECT *
+        FROM songs
+        WHERE name = ?
+        LIMIT 1
+        """
+        
+        song = CURSOR.execute(sql, (name,)).fetchone()
+
+        return cls.new_from_db(song)
